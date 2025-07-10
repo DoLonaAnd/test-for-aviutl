326b4b630a7c2a33db2d480a554609e7c43e51ef
Lona <103243217+DoLonaAnd@users.noreply.github.com>
2025-07-10 20:01:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function al3(){
   alert("外部スクリプト！");
 }
+
+function writeIn(num) {
+      try{
+      const p = document.getElementById("input-shower");
+      const inp = document.getElementById("in" + num).value;
+      p.innerHTML = inp;
+      alert(inp);
+      }catch(e) {
+        alert(e);
+      }
+    }
+
+  document.getElementById("in2").addEventListener("change", (event) => {
+    writeIn(2);
+  });
