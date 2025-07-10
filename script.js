0a8f58db6994af9e5fba2c07fef9941fe02128de
Lona <103243217+DoLonaAnd@users.noreply.github.com>
2025-07-10 20:50:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function al3(){
   alert("外部スクリプト！");
 }
 
-function writeIn(num) {
-    const p = document.getElementById("input-shower");
-    const inp = document.getElementById("in" + num).value;
-    p.innerHTML = inp;
-    alert(inp);
-    alert(e);
-}
+// function writeIn(num) {
+//     const p = document.getElementById("input-shower");
+//     const inp = document.getElementById("in" + num).value;
+//     p.innerHTML = inp;
+//     alert(inp);
+//     alert(e);
+// }
